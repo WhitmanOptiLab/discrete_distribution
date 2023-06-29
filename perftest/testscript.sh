bae4b895505e42aac70259c837be71973ec70b52
kuhlkena <kuhlkena@whitman.edu>
2023-06-29 16:11:38 -0500

    added uniform dist tests

--- perftest/testscript.sh
+++ perftest/testscript.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 
+# HOW TO USE:
+# Ex:
+# bash testscript.sh [num tests] [wightnum]
+
 # Script to test variations of the weighted random selections
-numtests=1
-weightnum=100
+
+
 
 echo "Number of tests: $1" # first argument is num of tests
 echo "Weightnum: $2" # second argument is number of weights in data structure
-echo "WRS, Heap, Discrete, WRS Changing, Heap Changing, Discrete Changing" > results.csv
+echo "normal_static_wrs, normal_static_heap, normal_static_discrete, normal_changing_wrs, normal_changing_heap, uniform_static_wrs, uniform_static_heap, uniform_static_discrete, uniform_changing_wrs, uniform_changing_heap" > results.csv
 
+# Normal distribution static weight tests
 g++ -I../lib -O3 "-DWRSLIB=nonuniform_int_distribution<int>" "-DWEIGHTNUM=$2" -o test0 normal_static.cpp
 g++ -I../lib -O3 "-DWRSLIB=fast_random_selector<int>" "-DWEIGHTNUM=$2" -o test1 normal_static.cpp
 g++ -I../lib -O3 "-DWRSLIB=std::discrete_distribution<int>" "-DWEIGHTNUM=$2" -o test2 normal_static.cpp
+
+# Normal distribution changing weight tests
 g++ -I../lib -O3 "-DWRSLIB=nonuniform_int_distribution<int>" "-DWEIGHTNUM=$2" -o test3 normal_changing.cpp
 g++ -I../lib -O3 "-DWRSLIB=fast_random_selector<int>" "-DWEIGHTNUM=$2" -o test4 normal_changing.cpp
+
+# Uniform distribution static weight tests
+g++ -I../lib -O3 "-DWRSLIB=nonuniform_int_distribution<int>" "-DWEIGHTNUM=$2" -o test5 uniform_static.cpp
+g++ -I../lib -O3 "-DWRSLIB=fast_random_selector<int>" "-DWEIGHTNUM=$2" -o test6 uniform_static.cpp
+g++ -I../lib -O3 "-DWRSLIB=std::discrete_distribution<int>" "-DWEIGHTNUM=$2" -o test7 uniform_static.cpp
+
+# Uniform distribution changing weight tests
+g++ -I../lib -O3 "-DWRSLIB=nonuniform_int_distribution<int>" "-DWEIGHTNUM=$2" -o test8 uniform_changing.cpp
+g++ -I../lib -O3 "-DWRSLIB=fast_random_selector<int>" "-DWEIGHTNUM=$2" -o test9 uniform_changing.cpp
 
 sum0=0
 sum1=0
 sum2=0
 sum3=0
 sum4=0
+sum5=0
+sum6=0
+sum7=0
+sum8=0
+sum9=0
 
 for ((i=1; i<=$1; i++)); do
     # Run your program and capture the output in a variable
@@ -27,6 +48,11 @@ for ((i=1; i<=$1; i++)); do
     result2=$(./test2)
     result3=$(./test3)
     result4=$(./test4)
+    result5=$(./test5)
+    result6=$(./test6)
+    result7=$(./test7)
+    result8=$(./test8)
+    result9=$(./test9)
 
     # append test to results.csv
     echo "$result0, $result1, $result2, $result3, $result4" >> results.csv
@@ -37,6 +63,11 @@ for ((i=1; i<=$1; i++)); do
     sum2=$(awk "BEGIN { printf \"%.6f\", $sum2 + $result2 }")
     sum3=$(awk "BEGIN { printf \"%.6f\", $sum3 + $result3 }")
     sum4=$(awk "BEGIN { printf \"%.6f\", $sum4 + $result4 }")
+    sum5=$(awk "BEGIN { printf \"%.6f\", $sum5 + $result5 }")
+    sum6=$(awk "BEGIN { printf \"%.6f\", $sum6 + $result6 }")
+    sum7=$(awk "BEGIN { printf \"%.6f\", $sum7 + $result7 }")
+    sum8=$(awk "BEGIN { printf \"%.6f\", $sum8 + $result8 }")
+    sum9=$(awk "BEGIN { printf \"%.6f\", $sum9 + $result9 }")
 done
 
 # Calculate the average
@@ -45,14 +76,24 @@ average1=$(awk "BEGIN { printf \"%.6f\", $sum1 / $1 }")
 average2=$(awk "BEGIN { printf \"%.6f\", $sum2 / $1 }")
 average3=$(awk "BEGIN { printf \"%.6f\", $sum3 / $1 }")
 average4=$(awk "BEGIN { printf \"%.6f\", $sum4 / $1 }")
+average5=$(awk "BEGIN { printf \"%.6f\", $sum5 / $1 }")
+average6=$(awk "BEGIN { printf \"%.6f\", $sum6 / $1 }")
+average7=$(awk "BEGIN { printf \"%.6f\", $sum7 / $1 }")
+average8=$(awk "BEGIN { printf \"%.6f\", $sum8 / $1 }")
+average9=$(awk "BEGIN { printf \"%.6f\", $sum9 / $1 }")
 
 # Add averages to the csv
-echo " , , , , , " >> results.csv
-echo "$average0, $average1, $average2, $average3, $average4" >> results.csv
+echo " " >> results.csv
+echo "$average0, $average1, $average2, $average3, $average4, $average5, $average6, $average7, $average8, $average9" >> results.csv
 
 # Print the average
-echo "wrs average time: $average0"
-echo "heap average time: $average1"
-echo "std average time: $average2"
-echo "wrs changing average time: $average3"
-echo "heap changing average time: $average4"
+echo "normal_static_wrs: $average0"
+echo "normal_static_heap: $average1"
+echo "normal_static_discrete: $average2"
+echo "normal_changing_wrs: $average3"
+echo "normal_changing_heap: $average4"
+echo "uniform_static_wrs: $average5"
+echo "uniform_static_heap: $average6"
+echo "uniform_static_discrete: $average7"
+echo "uniform_changing_wrs: $average8"
+echo "uniform_changing_heap: $average9"
---
+++ perftest/uniform_static.cpp
@@ -0,0 +1,48 @@
+//HOW TO USE COMPILATION FLAGS:
+//Enter the library you want to test after 'DWRSLIB=' and the number of weights after 'DWEIGHTNUM='.
+
+//Ex:
+//g++ -I../lib -O3 "-DWRSLIB=nonuniform_int_distribution<int>" "-DWEIGHTNUM=10000000" -o test0 uniform_static.cpp
+//g++ -I../lib -O3 "-DWRSLIB=heap_random_selector<int>" "-DWEIGHTNUM=100000" -o test1 uniform_static.cpp
+//g++ -I../lib -O3 "-DWRSLIB=std::discrete_distribution<int>" "-DWEIGHTNUM=100" -o test2 uniform_static.cpp
+
+#include "random_selector.hpp"
+#include "modifiable_heap_random_selector.hpp"
+#include <sys/time.h>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+using namespace dense::stochastic;
+
+int main() {
+  std::uniform_real_distribution<float> d(1,10); 
+  std::default_random_engine generator;
+  std::vector<float> weights = {};
+  int sum = 0;
+  
+  for(int i = 0; i < WEIGHTNUM; i++){
+    weights.push_back(d(generator));
+  }	      
+
+  float minweight = *std::min_element(weights.begin(), weights.end());
+  for(int i = 0; i < WEIGHTNUM; i++){
+    weights[i] -= minweight;
+  }	      
+
+  //start time
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
+
+  WRSLIB selector(weights.begin(), weights.end()); 
+  for (int i = 0; i < 100000; i++) {
+    sum += selector(generator);
+  }
+  
+  // end time
+  gettimeofday(&end, NULL);
+  double elapsedtime_sec = double(end.tv_sec - start.tv_sec) + 
+    double(end.tv_usec - start.tv_usec)/1000000.0;
+  std::cout << elapsedtime_sec << std::endl;
+  if(sum == 0 ) {std::cout << "hit"; } // Added conditional so compiler must compute sum
+}
