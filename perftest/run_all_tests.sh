430511111195db821066b02c839fd97deb3b66f9
kuhlkena <kuhlkena@whitman.edu>
2024-04-21 20:19:38 -0700

    switched which test we are using

--- perftest/run_all_tests.sh
+++ perftest/run_all_tests.sh
@@ -1,6 +1,6 @@
 echo "SELECTNUM, normal_static_wrs, normal_static_heap, normal_changing_wrs, normal_changing_heap, weibull_static_wrs, weibull_static_heap, weibull_changing_wrs, weibull_changing_heap" > results.csv
-for i in 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
-#for i in 10 100 1000 10000 100000 1000000 10000000
+#for i in 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
+for i in 10 100 1000 10000 100000 1000000 10000000
 do
   ./testscript.sh 10 $i
 done
