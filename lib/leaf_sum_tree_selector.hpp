5de0a95bcee40b8cb8ecbc44e4792d04b6cf09f9
Devonian <30977153+Devoniano@users.noreply.github.com>
2025-07-25 12:28:40 -0700

    Reorganized code and added STD requirements
    All required functions except for param() are now implemented and documented in the non-split leaf-sum tree. Several functions have been moved to private to allow the user to only interact in approved ways, and several functions have been removed due to no longer being needed.

--- lib/leaf_sum_tree_selector.hpp
+++ lib/leaf_sum_tree_selector.hpp
@@ -2,31 +2,32 @@
 #define LEAF_SUM_TREE
 #include "completetree.hpp"
 #include <limits>
-#include <utility>
 #include <random>
 #include <cassert>
-#include <iostream>
 
 namespace dense {
 namespace stochastic {
 
 
-//Class to randomly select an index where each index's probability of being 
-//  selected is weighted by a given vector.  
+//Class to randomly select an index where each index's probability of being...
+//...selected is weighted by a given vector.
 template <class IntType = size_t, size_t precision = std::numeric_limits<Real>::digits>
 class leaf_sum_tree : protected complete_tree<IntType, Real> {
- public:
-  using This = leaf_sum_tree<IntType, precision>;
-  using BaseTree = complete_tree<IntType, Real>;
-  using PosType = typename BaseTree::position_type;
-  static PosType left_of(PosType i) { return BaseTree::left_of(i);}
-  static PosType right_of(PosType i) { return BaseTree::right_of(i);}
-  static PosType parent_of(PosType i) { return BaseTree::parent_of(i);}
+using This = leaf_sum_tree<IntType, precision>;
+using BaseTree = complete_tree<IntType, Real>;
+using PosType = typename BaseTree::position_type;
 
-  //Weights can be of any type, but must be convertable to Real values
-  leaf_sum_tree() = delete;
-  leaf_sum_tree(PosType p) : BaseTree(p) {};
+//Methods accessible to the end user
+public:
 
+
+
+////MEMBER FUNCTIONS////
+
+  //Empty constructor, allows you to add later items to it
+  leaf_sum_tree() : BaseTree() {};
+
+  //Main constructor - takes iterators to the start and end of an array of weights
   template< class InputIt >
   leaf_sum_tree(InputIt first, InputIt last) :
     BaseTree()
@@ -39,34 +40,20 @@ class leaf_sum_tree : protected complete_tree<IntType, Real> {
     InputIt it = first;
     for (size_t i = 0; it != last; ++it, ++i) {
       BaseTree::value_of(leaf_start + i) = Real(*it);
-      //_total_weight += Real(*it);
     }
 
     //build sums from leaves up
     for (std::ptrdiff_t i = leaf_start - 1; i >= 1; --i) {
-      //Real tempval = BaseTree::value_of(2 * i) + BaseTree::value_of(2 * i + 1);
       BaseTree::value_of(i) = BaseTree::value_of(2 * i) + BaseTree::value_of(2 * i + 1);
     }
-    //compute_weights();
   }
 
+  //Reset doesn't need to do anything other than be callable
+  void reset();
 
-//  void compute_weights() {
-//    _total_weight = sum_weights(BaseTree::root());
-//  }
 
-  void update_weight(PosType i, Real new_weight) {
-    i=node_of(i);
-    Real weight_diff = new_weight - weightsum_of(i);
-    while (i != BaseTree::root()) {
-      weightsum_of(i) += weight_diff;
-      i = BaseTree::parent_of(i);
-    }
-    weightsum_of(i) += weight_diff;
-    //_total_weight += weight_diff;
-  }
 
-  //Real total_weight() const { return _total_weight; }
+////GENERATION////
 
   template<class URNG>
   PosType operator()(URNG& g) {
@@ -74,7 +61,7 @@ class leaf_sum_tree : protected complete_tree<IntType, Real> {
 
     PosType node = BaseTree::root();
 
-    while (node < get_leaf_start()) {
+    while (node < leaf_start) {
       //std::cout << "Target: " << target << std::endl;
 
       PosType left = BaseTree::left_of(node);
@@ -87,30 +74,58 @@ class leaf_sum_tree : protected complete_tree<IntType, Real> {
       //std::cout << std::endl;
     }
     //std::cout << "Returning node " << node << ", which has weight: " << weightsum_of(node) << std::endl;
-    
+
     return id_of(node);
   }
 
-  Real total_weight() const {
-    return BaseTree::value_of(1);  //root
+
+
+////CHARACTERISTICS////
+
+  //Returns a list of the probabilities of landing on each of the indexes
+  std::vector<double> probabilities(){
+    std::vector<double> probabilities(leaf_end);
+    double total_weight = BaseTree::value_of(1);
+    for(int i = leaf_start, j = 0; i < BaseTree::size(); ++i, ++j){
+      probabilities[j] = BaseTree::value_of(i)/total_weight;
+    }
+    return probabilities;
   }
 
-  Real& weight_of(PosType i) {
-    return BaseTree::value_of(i);
+  //TODO: param()
+  //param_type param() const {}
+  //void param(const param_type& params){}
+
+  //Returns the minimum index possible (always 0)
+  IntType min() const { return 0;}
+
+  //Returns the highest index possible
+  IntType max() const { return leaf_end-1;}
+
+
+
+////NON-MEMBER FUNCTIONS////
+
+  //Set a given leaf's weight to any positive real value
+  void update_weight(PosType i, Real new_weight) {
+    assert(new_weight >= 0);
+    i=node_of(i);
+    Real weight_diff = new_weight - weightsum_of(i);
+    while (i != BaseTree::root()) {
+      weightsum_of(i) += weight_diff;
+      i = BaseTree::parent_of(i);
+    }
+    weightsum_of(i) += weight_diff;
   }
 
+  //Gets a leaf's weight. Needed for this class because weights can be changed, and so...
+  //...the weights in the class can be different from those originally inputted
   Real get_weight(PosType i) const {
     assert(i >= 0 && i < static_cast<PosType>(leaf_start));
     return BaseTree::value_of(leaf_start + i);
   }
 
-  Real& weightsum_of(PosType p) {
-    return BaseTree::value_of(p);
-  }
-  const Real& weightsum_of(PosType p) const {
-    return const_cast<This*>(this)->weightsum_of(p);
-  }
-
+  //Adds a weight to the tree. If the tree is large enough to handle it, the weight is just added, but otherwise the tree has to double its size (costly).
   void push_back(Real& new_weight) {
     size_t new_leaf_end = leaf_end + 1;
     if (new_leaf_end > leaf_start) {
@@ -137,6 +152,7 @@ class leaf_sum_tree : protected complete_tree<IntType, Real> {
     }
   }
 
+  //Adds a weight to the tree. If the tree is large enough to handle it, the weight is just added, but otherwise the tree has to double its size (costly).
   void push_back(Real&& new_weight) {
     size_t new_leaf_end = leaf_end + 1;
     if (new_leaf_end > leaf_start) {
@@ -163,12 +179,14 @@ class leaf_sum_tree : protected complete_tree<IntType, Real> {
     }
   }
 
+  //Removes the last inserted weight (whatever's at the end of the input array).
+  //If the new size is half or less of the tree's current memory size, then the tree will rebuild to shrink to that size.
   void pop_back() {
     if (leaf_end == 0) return;
 
     size_t item_to_remove_idx = leaf_end - 1;
 
-    //First, efficiently set the weight to 0. This updates parents.
+    //First, set the weight to 0. This updates parents.
     update_weight(item_to_remove_idx, 0.0);
     --leaf_end;
 
@@ -277,21 +295,51 @@ class leaf_sum_tree : protected complete_tree<IntType, Real> {
     }
   }
 
-  PosType id_of(PosType p) { return p-leaf_start; }
 
-  PosType node_of(IntType i){return leaf_start+i;}
 
-  size_t get_leaf_start() const { return leaf_start; }
-
+//Methods only for use by the class itself
 private:
-  size_t leaf_start;
-  size_t leaf_end;
 
+  //Return the value stored in the root, which is the sum of all values in the tree
+  Real total_weight() const {
+    return BaseTree::value_of(1);  //root
+  }
+
+  //Raw value in any given tree (not user) index. Is a weight if >= leaf_start, is a weightsum if less than leaf_start
+  Real& weightsum_of(PosType p) {
+    return BaseTree::value_of(p);
+  }
+
+  //Const version
+  const Real& weightsum_of(PosType p) const {
+    return const_cast<This*>(this)->weightsum_of(p);
+  }
+
+  //Gets a user-visible ID from a leaf node
+  PosType id_of(PosType p) {
+    assert(p>=leaf_start);
+    return p-leaf_start; }
+
+  //Gets an internal tree node from a user-visible ID
+  PosType node_of(IntType i){
+    assert(i>=0 && i<leaf_end);
+    return leaf_start+i;}
+
+  //Returns the next largest power of two. Used in constructing the tree, either in the constructor or when rebuilding to be larger or smaller
   static size_t next_power_of_two(size_t n) {
     size_t p = 1;
     while (p < n) p <<= 1;
     return p;
   }
+
+  //Position functions used to navigate the tree.
+  static PosType left_of(PosType i) { return BaseTree::left_of(i);}
+  static PosType right_of(PosType i) { return BaseTree::right_of(i);}
+  static PosType parent_of(PosType i) { return BaseTree::parent_of(i);}
+
+  //Used for keeping track of indexing properly.
+  size_t leaf_start;
+  size_t leaf_end;
 };
 
 }
