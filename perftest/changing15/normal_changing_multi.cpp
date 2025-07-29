e205079d417152a4c4603fd43105ff742fd09ad4
burnsa2024 <awburns2024@gmail.com>
2025-07-29 11:12:08 -0700

    minor changes to testscripts, sideways fenwick update
    changing number of iterations, commenting out heap because we can exclude heap tests to save time, CHANGING SIDEWAYS FENWICK TO USE BIT_CAST IN UPDATE

--- perftest/changing15/normal_changing_multi.cpp
+++ perftest/changing15/normal_changing_multi.cpp
@@ -37,8 +37,8 @@ int main() {
   gettimeofday(&start, NULL);
   
   for (int i = 0; i < 1000000; i++) {
-    int index = selector(generator);
-    selector.update_weight(index, std::max<float>(0.0, d(generator)));
+    //int index = selector(generator);
+    //selector.update_weight(index, std::max<float>(0.0, d(generator)));
     for(int j = 0; j < 14; j++){
       selector.update_weight(randomIndex(generator), std::max<float>(0.0, d(generator)));    
     }
